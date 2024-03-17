df392111416165dfa39dcd0b82a60217680b7ee6
Jorge S. Sanchez <georotzen@gmail.com>
2024-03-16 21:42:55 -0700

    changes for v2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Georotzen Web Solutions 2024 on AWS Amplify Hosting Services: v2</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
