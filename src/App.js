9b0b3aec5b60cd98bba7ccf26fecb9951a87f86a
Jorge S. Sanchez <georotzen@gmail.com>
2024-03-16 21:55:27 -0700

    changes for App.js v2 March 16 2024

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import geoimage from './JorgeSScientist.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={geoimage} className="App" alt="logo" />
         <h2>Welcome to Georotzen Web Solutions 2024 on AWS Amplify Hosting Services: v2</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
